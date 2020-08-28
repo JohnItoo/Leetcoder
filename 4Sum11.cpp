464b7c98ea9b0d03254270dc01213e0a6a644424
JohnOhue <itoohue@gmail.com>
2020-08-28 16:00:07 +0100

    2020-08-28

---
+++ 4Sum11.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        int n = A.size();
+        if(n == 0) return n;
+        sort(A.begin(), A.end());
+                sort(B.begin(), B.end());
+                sort(C.begin(), C.end());
+                sort(D.begin(), D.end());
+
+        set<tuple<int, int, int> > sta;
+        set<tuple<int, int, int> > stb;
+        set<int> st;
+    
+        int ct = 0;
+        int xx = -1;
+        
+        while( xx+1 < n && A[xx+1] == 0 && B[xx+1] == 0 && C[xx+1] == 0 && D[xx+1] == 0) {
+            xx++;
+        }
+        ct = xx+1;
+        for(int i = xx+1; i < n; i++) {
+            for(int j = xx+1; j < n; j++) {
+                int smx = A[i] + B[j];
+                int smy = C[i] + D[j];
+                st.insert(smx);
+                st.insert(smy);
+                sta.emplace(smx, i,j);
+                stb.emplace(smy, i,j);
+            }
+        }
+        int ans = 0;
+        int q = 0;
+        ans = ct*ct*ct*ct;
+                     while(!sta.empty()) {
+            tuple<int, int, int> curra = *sta.begin();
+            int frst = get<0>(curra);
+            tuple<int, int, int> fnd = make_tuple(-frst, -1, -1);
+            sta.erase(sta.begin());
+            
+            auto it = stb.lower_bound(fnd);
+            
+            while(it != stb.end()) {
+                tuple<int,int,int> currb = *it;
+                if(get<0>(currb) != -frst) break;
+                ans++;
+                ++it;
+
+            }
+            q++;
+        }
+       
+        return ans;
+        
+    }
+};
