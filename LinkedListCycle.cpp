996a399080e2bdc8aba45b27496e70e01ded14d3
JohnOhue <itoohue@gmail.com>
2020-09-04 01:00:05 +0100

    2020-09-04

--- LinkedListCycle.cpp
+++ LinkedListCycle.cpp
@@ -10,18 +10,42 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         map<ListNode*, int> nodes;
-        
+
         bool ans = false;
-        
-        while(head != NULL) {
-            if(nodes.find(head) != nodes.end()) {
+
+        while (head != NULL) {
+            if (nodes.find(head) != nodes.end()) {
                 ans = true;
                 break;
             }
             nodes[head] = 1;
             head = head->next;
         }
-        
+
         return ans;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* tortise = head;
+        ListNode* hare = head;
+        while (hare != NULL && hare->next != NULL) {
+            tortise = tortise->next;
+            hare = hare->next->next;
+            if (tortise == hare) {
+                return true;
+            }
+        }
+        return false;
     }
 };
