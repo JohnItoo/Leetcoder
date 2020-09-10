36ad17b922ea4ef9085db72c3ecd61fb1bd95765
JohnOhue <itoohue@gmail.com>
2020-09-10 10:00:07 +0100

    2020-09-10

--- ReverseVowelsOfAString.cpp
+++ ReverseVowelsOfAString.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     string reverseVowels(string a) {
-        char arr[10] = {'A', 'a', 'e' ,'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+        char arr[10] = {'A', 'a', 'e' , 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
         map<char, int> mp;
-        for(char vowel : arr) {
+        for (char vowel : arr) {
             mp[vowel]++;
         }
         int n = a.length();
-        int i = 0; int j = n-1;
-        
-        while(i < j) {
-            if(mp.find(a[i]) == mp.end()) {
+        int i = 0; int j = n - 1;
+
+        while (i < j) {
+            if (mp.find(a[i]) == mp.end()) {
                 i++;
             }
-            if(mp.find(a[j]) == mp.end()) {
+            if (mp.find(a[j]) == mp.end()) {
                 j--;
             }
-            
-            if(mp.find(a[j]) != mp.end() && mp.find(a[i]) != mp.end()) {
+
+            if (mp.find(a[j]) != mp.end() && mp.find(a[i]) != mp.end()) {
                 swap(a[i], a[j]);
                 i++; j--;
-            } 
+            }
         }
-        
+
         return a;
-        
-        
+
+
     }
 };
