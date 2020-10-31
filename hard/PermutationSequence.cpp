0df14407fc2a74c3cd85dc29fc12814083c21280
JohnOhue <itoohue@gmail.com>
2020-10-31 10:00:05 +0100

    2020-10-31

--- hard/PermutationSequence.cpp
+++ hard/PermutationSequence.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     string getPermutation(int n, int k) {
         string sequence;
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             char c = i + '0';
             sequence.push_back(c);
         }
         int i = 1;
         do {
-            if(i == k) break;
-          i+=1;  
-        } while(next_permutation(sequence.begin(), sequence.end()) && i < k);
-       return sequence;
+            if (i == k) break;
+            i += 1;
+        } while (next_permutation(sequence.begin(), sequence.end()) && i < k);
+        return sequence;
     }
 };
