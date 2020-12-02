32238e26f00f1f50fb3ff7e96572b5b2e9fcf87d
JohnOhue <itoohue@gmail.com>
2020-12-02 17:00:06 +0100

    2020-12-02

---
+++ AmazonFreshPromotion.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	vector<string> shoppingCart(n);
+	for(int i = 0; i < n; i++) cin >> shoppingCart[i];
+    int m; cin >> m;
+    vector<vector<string> >  codeList(m);
+	for(int i = 0; i < m; i++) {
+		int len; cin >> len;
+	}
+	return 0;
+}
