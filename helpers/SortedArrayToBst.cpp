abb94452eae9f32a01bf3a0ea73aa57d21aa7f18
JohnOhue <itoohue@gmail.com>
2020-12-31 20:00:06 +0100

    2020-12-31

--- helpers/SortedArrayToBst.cpp
+++ helpers/SortedArrayToBst.cpp
@@ -1,39 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TreeNode {
-    public: 
+struct TreeNode
+{
+public:
     int val;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
 
     TreeNode(int val) : val(val), left(NULL), right(NULL) {}
-    TreeNode(int val, TreeNode* left, TreeNode* right) : val(val), left(left), right(right) {}
+    TreeNode(int val, TreeNode *left, TreeNode *right) : val(val), left(left), right(right) {}
 };
 
-TreeNode* sortedArrayToBst(vector<int> a, int start, int end) {
-    if(start > end) {
+TreeNode *sortedArrayToBst(vector<int> a, int start, int end)
+{
+    if (start > end)
+    {
         return NULL;
     }
-    int mid = (start + end)/2;
-    struct TreeNode* root = (struct TreeNode*) malloc(sizeof(struct TreeNode*));
-    root->left = sortedArrayToBst(a, start, mid-1);
-    root->right = sortedArrayToBst(a, mid+1, end);
-    root-> val = a[mid];
+    int mid = (start + end) / 2;
+    struct TreeNode *root = (struct TreeNode *)malloc(sizeof(struct TreeNode *));
+    root->left = sortedArrayToBst(a, start, mid - 1);
+    root->right = sortedArrayToBst(a, mid + 1, end);
+    root->val = a[mid];
     return root;
 }
-void preorder(TreeNode* root) {
-    if(root == NULL) return;
+void preorder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
     cout << root->val << endl;
     preorder(root->left);
     preorder(root->right);
     cout << endl;
 }
 
-int main() {
+int main()
+{
     vector<int> a;
-    for(int i = 1; i <= 4; i++) a.push_back(i);
-    TreeNode* root = sortedArrayToBst(a, 0, a.size()-1);
+    for (int i = 1; i <= 4; i++)
+        a.push_back(i);
+    TreeNode *root = sortedArrayToBst(a, 0, a.size() - 1);
     return 0;
     preorder(root);
 
