63b982218eda413f34b4a9e8529cc295cb2d88ca
JohnOhue <itoohue@gmail.com>
2021-01-09 04:00:05 +0100

    2021-01-09

--- helpers/AtMostKJumps.cpp
+++ helpers/AtMostKJumps.cpp
@@ -9,14 +9,13 @@ int main() {
     dp[2][1] = 1;
 
     for(int step = 3; step <= n; step++) {
-        for(int jump = 2; jump <= k; jump++) {
-           for(int i = 0; i <= k; i++) {
-               dp[step][jump] += dp[step-1][i] + dp[step-2][i];
-           }
+        for(int jump = 1; jump <= k; jump++) {
+            dp[step][jump] += dp[step-1][jump-1] + dp[step-2][jump-1];
+    
         }
     }
     for(int jump = 0; jump <= k; jump++) {
-        cout << dp[3][jump] << " ";
+        cout << dp[5][jump] << " ";
     }
     cout << endl;
 
