8b6d233db3168460ec1bd4e5a74db05a43dec480
JohnOhue <itoohue@gmail.com>
2021-07-26 13:00:05 -0400

    2021-07-26

--- medium/MinimumRemoveToMakeValidParantheses.cpp
+++ medium/MinimumRemoveToMakeValidParantheses.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    string minRemoveToMakeValid(string s) {
+    string cleanCloseAtBeginOpenAtEnd(string s) {
+        int i = 0;
         int n = s.size();
+        while(i < n && s[i] == ')') {
+            i++;
+        }
+        
+        int j = n-1;
+        while(j >= 0 && s[j] == '(' ) {
+            j--;
+        }
+        string res;
+        if(i < n) {
+            res = s.substr(i, j-i+1);
+        }
+
+        return res;
+        
+    }
+    string minRemoveToMakeValid(string s) {
         int val = 0;
         unordered_set<int> bad;
         
         
-        for(int i = n-1; i >= 0; i--) {
-            if(s[i] == ')') break;
-            if(s[i] == '(') {
-                bad.insert(i);
-            }
-        }
-        
-        for(int i = 0; i < n; i++) {
-            if(s[i] == '(') break;
-            if(s[i] == ')') {
-                bad.insert(i);
-            }
-        }
-        
-        
-        
-        string res;
-        
-        for(int i = 0; i < n; i++) {
-            if(s[i] != ')' && s[i] != '(') {
-                res.push_back(s[i]);
-            } else {
-                if(bad.find(i) == bad.end()){
-                        res.push_back(s[i]);
-               }
-            }
-        }
-        
-        
-        s = res;
-        n = res.size();
-        bad.clear();
+        s =  cleanCloseAtBeginOpenAtEnd(s);
+        int n = s.size();
 
         
         for(int i = 0; i < n; i++) {
@@ -51,11 +39,11 @@ public:
                 }
             }
         }
-        res = "";
+       string res = "";
         
         for(int i = n-1; i >= 0; i--) {
             if(s[i] == '(') {
-                if(val) val--;
+                if(val) val--; // clean extra '(' from the back.
                 else{
                     res.push_back(s[i]);
                 }
